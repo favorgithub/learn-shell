9cb081346f3d593a6928a841f05bfa78b404eabc
favorgithub <gloify@yahoo.com>
2023-06-01 11:02:42 -0500

    roboshop nginx servers

--- 03-var.sh
+++ 03-var.sh
@@ -5,3 +5,10 @@ echo value of a is $a dollars
 #Alternatively variables also can be accessed by using ${a}
 
 echo value of a is ${a} dollars
+
+# Special variables for inputs
+# Special variables used for inputs are $0 to $n, $*, $#
+echo $0
+echo $1
+echo $*
+echo $#

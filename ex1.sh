38d36cad6d498881881226491dc05786c34323bb
favorgithub <gloify@yahoo.com>
2023-06-02 18:32:31 -0500

    roboshop nginx servers

---
+++ ex1.sh
@@ -0,0 +1,6 @@
+for component in frontend mongodb catalogue redis user cart msysql shipping rabbitmq payment dispatch ; do
+  echo Creating Server - $Component
+
+  $ aws ec2 run-instances --image-id ami-08ee87f57b38db5af --instance-type t3.small
+
+done
